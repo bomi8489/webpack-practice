fd8efc109d7fd9eecfe72dc685f4d8ca666b25c8
AUTUMN <krshlee22@naver.com>
2023-04-21 18:53:01 +0900

    webpack practice

--- src/components/Container/Note.js
+++ src/components/Container/Note.js
@@ -2,17 +2,22 @@ import React, { useEffect, useRef, useState } from 'react'
 import NoteContents from './NoteContents'
 
 function Note() {
-    const [text, setText] = useState("");
+    const [text, setText] = useState("새로운 노트");
     const [noteList, setNoteList] = useState([]);
     const textareaRef = useRef();
-    const textareaBoxRef = useRef();
+
+    const note = {
+        id: Date.now(),
+        data: text
+    }
 
     const onChange = (e) => {
         setText(e.target.value);
     }
 
     const clickAdd = () => {
-        setNoteList(prev => [...prev, text]);
+        setNoteList(prev => [...prev, note]);
+        setText("새로운 노트");
         textareaRef.current.disabled = false;
         textareaRef.current.value = text;
         textareaRef.current.className = 'm-[20px] h-[68px] w-[840px] resize-none outline-none bg-white';
@@ -28,7 +33,7 @@ function Note() {
     }
 
     useEffect(() => {
-        setText("새로운 노트");
+        console.log(noteList)
     }, noteList)
 
     return (
@@ -44,8 +49,8 @@ function Note() {
             </div>
             <div className='bg-black mb-7 w-[880px] flex flex-wrap'>
                 {noteList.length >= 1 && 
-                noteList.map(noteText => (
-                    <NoteContents noteText={noteText}/>
+                noteList.map(_ => (
+                    <NoteContents textareaRef={textareaRef} text={text}/>
                 ))
                 }
                 <div onClick={clickAdd} className='h-[160px] w-[160px] bg-[#C9CAC9] rounded-[15px] m-[8px] hover:cursor-pointer'></div>
